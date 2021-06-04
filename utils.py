6440dbb03074137539ea25574f21199e4bf75aa4
Amine M. Boulouma <amine@boulouma.com>
2021-06-04 16:19:12 +0200

    Update

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def read_input(filename):
     temporal_requirements_matrix = []
 
     #ShiftName, Start, Length, MinlengthOfBlocks, MaxLengthOfBlocks
-    shift_name, start, length, min_length_of_blocks, max_length_of_blocks \
+    shift_name, start_shift, length_shift, min_length_of_blocks, max_length_of_blocks \
         = [], [], [], [], []
 
     # Minimum and maximum length of days-off blocks 
@@ -48,12 +48,12 @@ def read_input(filename):
                 temporal_requirements_matrix = []
                 for i in range(number_of_shifts):
                     temporal_requirements_matrix.append(str(next(lines)))
-            if "#ShiftName, Start, Length, MinlengthOfBlocks, MaxLengthOfBlocks" in line:
+            if "#ShiftName" in line:
                 ns = number_of_shifts
-                shift_name, start, length, min_length_of_blocks, max_length_of_blocks \
+                shift_name, start_shift, length_shift, min_length_of_blocks, max_length_of_blocks \
                     = ['-']*ns, [0]*ns, [0]*ns, [0]*ns, [0]*ns
                 for i in range(number_of_shifts):
-                    shift_name[i], start[i], length[i], min_length_of_blocks[i], max_length_of_blocks[i] = next(lines).split()
+                    shift_name[i], start_shift[i], length_shift[i], min_length_of_blocks[i], max_length_of_blocks[i] = next(lines).split()
             if "# Minimum and maximum length of days-off blocks" in line:
                 min_days_off, max_days_off = list(map(int, next(lines).split()))
             if "# Minimum and maximum length of work blocks" in line:
@@ -61,12 +61,11 @@ def read_input(filename):
             if "# Number of not allowed shift sequences: NrSequencesOfLength2, NrSequencesOfLength3:" in line:
                 nr_sequences_of_length_2, nr_sequences_of_length_3 = list(map(int, next(lines).split()))
             if "# Not allowed shift sequences" in line:
-                not_allowed_shif_sequences = []
-                not_allowed_shif_sequences.append(str(next(lines)))
-                # while(next(lines)):
-                #     not_allowed_shif_sequences.append(line)
-                # else: 
-                #     break
+                not_allowed_shift_sequences_2, not_allowed_shift_sequences_3 = [], []
+                for i in range(nr_sequences_of_length_2):
+                    not_allowed_shift_sequences_2.append(next(lines).split('\n')[0])
+                for i in range(nr_sequences_of_length_3):
+                    not_allowed_shift_sequences_3.append(next(lines).split('\n')[0])
 
     input_data = {
         'length_of_schedule': length_of_schedule,
@@ -74,8 +73,8 @@ def read_input(filename):
         'number_of_shifts': number_of_shifts, 
         'temporal_requirements_matrix': temporal_requirements_matrix,
         'shift_name': shift_name,
-        'start': start,
-        'length': length,
+        'start_shift': start_shift,
+        'length_shift': length_shift,
         'min_length_of_blocks': min_length_of_blocks,
         'max_length_of_blocks': max_length_of_blocks,
         'min_days_off': min_days_off,
@@ -84,9 +83,11 @@ def read_input(filename):
         'max_length_work_blocks': max_length_work_blocks,
         'nr_sequences_of_length_2': nr_sequences_of_length_2,
         'nr_sequences_of_length_3': nr_sequences_of_length_3,
-        'not_allowed_shif_sequences': not_allowed_shif_sequences    
+        'not_allowed_shift_sequences_2': not_allowed_shift_sequences_2,
+        'not_allowed_shift_sequences_3': not_allowed_shift_sequences_3    
     }
 
     return input_data 
 
 pprint(read_input('rws_instances/Example1.txt'))
+pprint(read_input('rws_instances/Example4.txt'))
